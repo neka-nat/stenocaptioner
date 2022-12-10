57bf3a8864be93d98c44396de439ecf8acc79424
nekanat <nekanat.stock@gmail.com>
2022-12-10 15:05:42 +0900

    add split string

--- stenocaptioner/utils.py
+++ stenocaptioner/utils.py
@@ -12,7 +12,7 @@ def split_en_sentences(text: str) -> list[str]:
 
 def split_ja_sentences(text: str) -> list[str]:
     sentences = []
-    for sentence in re.split(r"(?<=[、。？！])", text):
+    for sentence in re.split(r"(?<=[、。？！?!])", text):
         sentence = sentence.strip()
         if sentence:
             sentences.append(sentence)
@@ -32,7 +32,7 @@ def tweak_en_lines(lines: list[str]) -> list[str]:
 def tweak_ja_lines(lines: list[str]) -> list[str]:
     new_lines = []
     for line in lines:
-        if new_lines and line in ["、", "。", "？", "！"]:
+        if new_lines and line in ["、", "。", "？", "！", "?", "!"]:
             new_lines[-1] += line
         else:
             new_lines.append(line)
